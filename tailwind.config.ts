1fe48a3b0eba5b7dbe68993aa4a5e80fd92bac7c
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-18 19:01:41 +0530

    Tailwind & Dumby Pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "#252525",
       },
+      screens: {
+        tablet: "640px",
+        navHide: "800px",
+        laptop: "1024px",
+      },
+    },
+    fontFamily: {
+      heading: ["Monaco", "Bitstream Vera Sans Mono", "Lucida Console"],
     },
   },
   plugins: [],
-}
-export default config
+};
