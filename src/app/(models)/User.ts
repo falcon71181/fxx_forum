16d772286f1f71e8d5f1dfef5922e1bb782cda07
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-19 15:36:36 +0530

    User Model for DB

---
+++ src/app/(models)/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Model, Document } from "mongoose";
+
+const { Schema, model } = mongoose;
+
+export type UserType = {
+  email: string;
+  password: string;
+  date: Date;
+} & Document;
+
+const UserSchema = new Schema<UserType>({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+let User: Model<UserType>;
+
+try {
+  // Try to get the existing model
+  User = model("User") as Model<UserType>;
+} catch (error) {
+  // Define the model if it doesn't exist
+  User = model<UserType>("User", UserSchema);
+}
+
+export default User;
