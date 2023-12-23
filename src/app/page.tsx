c83ce7b484183a56d4dc112eb6c76b1b926cf217
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-23 17:49:30 +0530

    Initial Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen justify-center items-center text-6xl text-white">
-      / PAGE
+    <main className="pt-20 flex min-h-screen min-w-screen justify-center text-6xl text-slate-300">
+      <div className="w-4/5 h-screen shadow-2xl shadow-gray-600">
+        <nav className="w-full h-10 bg-gray-800 rounded-t-xl">
+          <ul className=" h-full flex justify-evenly">
+            <li>
+              <Link
+                href="/"
+                className="text-slate-300 hover:bg-slate-900 hover:text-white h-full flex items-center justify-center px-3 text-xl"
+              >
+                GENERAL
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="text-slate-300 hover:bg-slate-900 hover:text-white h-full flex items-center justify-center px-3 text-xl"
+              >
+                MEMES
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="text-slate-300 hover:bg-slate-900 hover:text-white h-full flex items-center justify-center px-3 text-xl"
+              >
+                TECH
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="text-slate-300 hover:bg-slate-900 hover:text-white h-full flex items-center justify-center px-3 text-xl"
+              >
+                ISSUE
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </main>
   );
 }
