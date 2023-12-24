ecfc0d333b2b64b71f7d0864f084f2705d9211be
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-24 15:21:54 +0530

    GET req Added to Scrape All Boards

--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -54,3 +54,37 @@ export async function POST(request: Request) {
     disconnectDB();
   }
 }
+
+export async function GET(request: Request) {
+  let boardList: any = {};
+
+  try {
+    // wait for DB to connect
+    await connectDB();
+
+    const allBoards = await Board.find({});
+
+    // Append data to boardList
+    boardList = { ...boardList, ...allBoards };
+    const responseBody = JSON.stringify(boardList);
+
+    return new Response(responseBody, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return new Response("Internal Server Error", {
+      status: 500,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  } finally {
+    // Disconnect from the database after the newUser operation is completed or in case of an error
+    disconnectDB();
+  }
+}
