2c21a004ef0e77e6d1d9c01fd72a472686ad36e0
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-27 00:04:10 +0530

    Validation API Added

---
+++ src/app/api/validate/route.ts
@@ -0,0 +1,68 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import User from "@/app/(models)/User";
+import { connectDB, disconnectDB } from "@/app/(lib)/mongoose";
+
+export async function POST(request: Request) {
+  const authorization = request.headers.get("authorization");
+  const JWT_Secret = process.env.JWT_SECRET as string;
+
+  type Payload = JwtPayload & { email: string };
+
+  if (!authorization) {
+    return new Response("No JWT Token ", {
+      status: 401,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+
+  // Split the Authorization header to get the token part
+  const [bearer, token] = authorization.split(" ");
+
+  // Check if the header is in the expected "Bearer <token>" format
+  if (bearer !== "Bearer" || !token) {
+    return new Response("Invalid Authorization header format", {
+      status: 401,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+
+  try {
+    await connectDB();
+
+    const payload = jwt.verify(token, JWT_Secret) as Payload;
+    const user = await User.findOne({ email: payload.email });
+
+    if (!user) {
+      const error = "User doesn't exist";
+      return new Response("User doesn't exist", {
+        status: 404,
+        headers: {
+          "Content-Type": "text/plain",
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ payload }), {
+      status: 200,
+      headers: {
+        "Content-Type": "text/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return new Response("Internal Server Error", {
+      status: 500,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  } finally {
+    // Make sure to disconnect from the database in case of any error
+    disconnectDB();
+  }
+}
