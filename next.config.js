f6897171b73f8880be9b1e28e06a7441c5115d12
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-27 21:18:17 +0530

    CORS fix try 1

--- next.config.js
+++ next.config.js
@@ -1,32 +1,33 @@
-// // next.config.js
-//
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     async headers() {
-//         return [
-//             {
-//                 // matching all API routes
-//                 source: "/api/:path*",
-//                 headers: [
-//                     { key: "Access-Control-Allow-Credentials", value: "true" },
-//                     { key: "Access-Control-Allow-Origin", value: "https://fxx-forum.vercel.app" },
-//                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-//                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//                 ]
-//             }
-//         ]
-//     }
-// }
-//
-// module.exports = nextConfig
-// next.config.js
-module.exports = {
-  async rewrites() {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://fxx-forum.vercel.app/:path*",
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
       },
     ];
   },
 };
+
+module.exports = nextConfig;
