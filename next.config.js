5d740839be2c0fd76736bb3f6885032422203230
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-27 21:12:37 +0530

    destination Added

--- next.config.js
+++ next.config.js
@@ -1,21 +1,32 @@
+// // next.config.js
+//
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     async headers() {
+//         return [
+//             {
+//                 // matching all API routes
+//                 source: "/api/:path*",
+//                 headers: [
+//                     { key: "Access-Control-Allow-Credentials", value: "true" },
+//                     { key: "Access-Control-Allow-Origin", value: "https://fxx-forum.vercel.app" },
+//                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+//                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//                 ]
+//             }
+//         ]
+//     }
+// }
+//
+// module.exports = nextConfig
 // next.config.js
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://fxx-forum.vercel.app" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
-}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://fxx-forum.vercel.app/:path*",
+      },
+    ];
+  },
+};
