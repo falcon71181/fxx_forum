72b046b0bf95444dcf749b4f5beb6463bc2f5620
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-27 19:19:34 +0530

    Part Time SOLUTION

--- src/app/(components)/board.tsx
+++ src/app/(components)/board.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link';
 import React from 'react';
 
 interface BoardProps {
+  _id: string;
   title: string;
   date: Date;
 }
 
 const BoardCard: React.FC<BoardProps> = (props) => {
   return (
-    <Link href="/">
+    <Link href={`/${props._id}`}>
       <section className='flex relative items-center border-2 border-blue-500 min-w-screen min-h-[80px] m-4 p-2 rounded-lg'>
         <div className='text-slate-300 font-extrabold text-3xl'>{props.title}</div>
         <div className='absolute right-5 bottom-2 text-slate-300 text-sm'>{props.date.toString()}</div>
--- src/app/(components)/boardCateg.tsx
+++ src/app/(components)/boardCateg.tsx
@@ -11,21 +11,22 @@ interface BoardCategoryProps {
   category: string;
 }
 
-const BoardCategory: React.FC<BoardCategoryProps> = ({ category }) => {
+const BoardCategory = () => {
   const [boards, setBoards] = useState<BoardData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await boardList();
-        setBoards(data[category]);
+        setBoards(data);
+        console.log(data);
       } catch (error: any) {
-        console.error(`Error fetching ${category} board list:`, error.message);
+        console.error(`Error fetching board list:`, error.message);
       }
     };
 
     fetchData();
-  }, [category]);
+  }, []);
 
   return (
     <div>
@@ -39,9 +40,38 @@ const BoardCategory: React.FC<BoardCategoryProps> = ({ category }) => {
     </div>
   );
 };
-
-export const GeneralCateg: React.FC = () => <BoardCategory category="general" />;
-export const MemeCateg: React.FC = () => <BoardCategory category="memes" />;
-export const TechCateg: React.FC = () => <BoardCategory category="tech" />;
-export const IssueCateg: React.FC = () => <BoardCategory category="issue" />;
-
+export default BoardCategory;
+// const BoardCategory: React.FC<BoardCategoryProps> = ({ category }) => {
+//   const [boards, setBoards] = useState<BoardData[]>([]);
+//
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const data = await boardList();
+//         setBoards(data[category]);
+//       } catch (error: any) {
+//         console.error(`Error fetching ${category} board list:`, error.message);
+//       }
+//     };
+//
+//     fetchData();
+//   }, [category]);
+//
+//   return (
+//     <div>
+//       {boards.map((boardData) => (
+//         <BoardCard
+//           key={boardData._id}
+//           title={boardData.title}
+//           date={new Date(boardData.date)}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+//
+// export const GeneralCateg: React.FC = () => <BoardCategory category="general" />;
+// export const MemeCateg: React.FC = () => <BoardCategory category="memes" />;
+// export const TechCateg: React.FC = () => <BoardCategory category="tech" />;
+// export const IssueCateg: React.FC = () => <BoardCategory category="issue" />;
+//
