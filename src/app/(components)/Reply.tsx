486e1eaa325ca7ff2f463c533ff552fd2c82c1b4
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-30 14:18:11 +0530

    Reply Card Added

---
+++ src/app/(components)/Reply.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface ReplyProps {
+  leader: string;
+  reply: string;
+  date: Date;
+}
+
+const ReplyCard: React.FC<ReplyProps> = (props) => {
+  const replyOwner = props.leader.split("@")[0];
+  return (
+    <main className="my-3 flex min-w-screen justify-center rounded-t-xl">
+      <div className="w-4/5 shadow-2xl shadow-cyan-300">
+        <section className="border-[1px] border-blue-500 w-full min-h-50 flex flex-col rounded-t-xl">
+          <div className="flex flex-row justify-between p-3 bg-gray-800 text-white h-10 rounded-t-xl">
+            <div className="font-heading font-medium h-full flex justify-center items-center">by @{replyOwner}</div>
+            <div className="h-full flex justify-center items-center">{props.date.toString()}</div>
+          </div>
+          <div className="text-sm text-slate-300 p-3 w-full h-full whitespace-pre-wrap hyphens-auto">
+            {props.reply}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default ReplyCard;
