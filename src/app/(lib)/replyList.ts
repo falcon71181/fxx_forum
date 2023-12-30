b53095051d87cfa25d4d249584d0e61e355cbe48
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-30 13:55:37 +0530

    reply fetch () Added

---
+++ src/app/(lib)/replyList.ts
@@ -0,0 +1,21 @@
+export const replyList = async (postId: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/reply/${postId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error: ${res.statusText}`);
+    }
+
+    const result = res.json();
+    console.log(result);
+    return result;
+  } catch (error: any) {
+    console.error("Error fetching reply list:", error.message);
+    throw error;
+  }
+};
