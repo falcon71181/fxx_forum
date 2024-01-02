8f82f0d002796898357e582eb19fa6cc6dd93dbc
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-02 15:38:58 +0530

    Responsive Footer

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <main className="flex justify-center items-center w-full h-24 font-heading text-slate-300">
-      &copy; 2024. Made with ❤️ by
-      <a href="https://github.com/falcon71181" target="_blank" className="mx-2 text-white">falcon71181</a>
+      &copy; 2024. <span className="hidden tablet:flex">Made with ❤️ by</span>
+      <a href="https://github.com/falcon71181" target="_blank" className="mx-2 text-white hidden tablet:flex">falcon71181</a>
     </main>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import NavBar from "./(components)/NavBar";
-import Footer from "./(components)/footer";
+import Footer from "./(components)/Footer";
 import { ReactNode } from 'react';
 
 export const metadata = {
